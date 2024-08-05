e494e48cc1b1552525aabd93880971faa8e09a84
Virut Ratinimittham <virut.r@7solutions.co.th>
2024-08-05 16:49:48 +0700

    line credential remove

--- line.py
+++ line.py
@@ -7,8 +7,7 @@ import datetime
 app = Flask(__name__)
 
 
-line_bot_api = LineBotApi('I9mgk1gqe7x4tirEd5AjNi5Il91E0iiAF44CeRU/kaQd+NatHpxtBXMNE6PbArDhuIptcyh77FDtueM9f+6gqKAAnVpqxPKKc4DsCNjWuK4IxK8xcxOmzn9j9YE8oavM1wc+o+8Uo8UDgT6uS9XSaQdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('514a4a0dbabf4aae1cac76ee28aed860')
+
 
 @app.route("/healthz", methods=['GET'])
 def health_check():
